d8db7cc15760f9084532cf5ade83d37f37527909
semih007 <134814989+semih007@users.noreply.github.com>
2026-05-18 21:12:09 +0000

    Hata düzeltildi: Maven repositories eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui:1.6.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material3:material3:1.3.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
