aa93358a0db0b6e929ddfed8992282bbb86478ac
semih007 <134814989+semih007@users.noreply.github.com>
2026-05-18 20:54:20 +0000

    xxx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,11 +47,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.material3:material3:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("android") version "1.9.22" apply false
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.5.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
