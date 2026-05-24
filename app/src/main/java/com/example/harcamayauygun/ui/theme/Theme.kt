bf100aba9e8446409bc659ff8ffef77f8b69441f
semih007 <134814989+semih007@users.noreply.github.com>
2026-05-24 12:12:20 +0000

    bla bla

--- app/src/main/java/com/example/harcamayauygun/ui/theme/Theme.kt
+++ app/src/main/java/com/example/harcamayauygun/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.harcamayauygun.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -24,9 +25,18 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun HarcamayaUygunTheme(content: @Composable () -> Unit) {
+fun HarcamayaUygunTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
     MaterialTheme(
-        colorScheme = LightColors,
+        colorScheme = colorScheme,
         typography = MaterialTheme.typography,
         content = content
     )
